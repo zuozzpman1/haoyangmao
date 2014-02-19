cfa39633457a7a85991f1949e38f12d5d5e80e05
zuozzpman1 <zuozzpman1@gmail.com>
2014-02-19 14:23:31 +0800

    提交MainActivity

--- src/com/haoyangmao/haoyangmao/MainActivity.java
+++ src/com/haoyangmao/haoyangmao/MainActivity.java
@@ -8,15 +8,12 @@ import com.haoyangmao.haoyangmao.common.net.NetObserver;
 import org.apache.http.HttpEntity;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.util.Locale;
-
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
